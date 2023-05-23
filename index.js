b15bbb5eed8385dbaa0ad999eb00fc16ad7fc9ff
Quinn <quinn@indra.ai>
2023-05-22 23:01:59 -0700

    prompt updates

--- index.js
+++ index.js
@@ -206,7 +206,7 @@ const LOG = new Deva({
     help(packet) {
       return new Promise((resolve, reject) => {
         this.lib.help(packet.q.text, __dirname).then(help => {
-          return this.question(`#FeectingDeva parse ${help}`);
+          return this.question(`#feecting parse ${help}`);
         }).then(parsed => {
           return resolve({
             text: parsed.a.text,
