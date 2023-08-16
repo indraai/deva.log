97a09f30cc98a2fa427ea3a0b59828ed41456854
Quinn <quinn@indra.ai>
2023-08-15 23:31:43 -0700

    update logging

--- index.js
+++ index.js
@@ -37,72 +37,11 @@ const LOG = new Deva({
   deva: {},
   func: {
     /**************
-    func: log_error
-    params: packet
-    describe: log the error in the machine.
-    ***************/
-    log_error(packet) {
-      const isSelf = this.agent().id == packet.agent.id;
-      this.func.log_write('error', packet);
-    },
-
-    /**************
-    func: log_zone
-    params: packet
-    describe: log the state changes in the machine.
-    ***************/
-    log_zone(packet) {
-      const isSelf = this.agent().id == packet.agent.id;
-      this.func.log_write('zone', packet);
-    },
-
-    /**************
-    func: log_feature
-    params: packet
-    describe: log the state changes in the machine.
-    ***************/
-    log_feature(packet) {
-      const isSelf = this.agent().id == packet.agent.id;
-      this.func.log_write('feature', packet);
-    },
-
-    /**************
-    func: log_action
-    params: packet
-    describe: log the state changes in the machine.
-    ***************/
-    log_action(packet) {
-      const isSelf = this.agent().id == packet.agent.id;
-      this.func.log_write('action', packet);
-    },
-
-    /**************
-    func: log_state
-    params: packet
-    describe: log the state changes in the machine.
-    ***************/
-    log_state(packet) {
-      const isSelf = this.agent().id == packet.agent.id;
-      this.func.log_write('state', packet);
-    },
-
-    /**************
-    func: log_state
-    params: packet
-    describe: log the state changes in the machine.
-    ***************/
-    log_context(packet) {
-      const isSelf = this.agent().id == packet.agent.id;
-      this.func.log_write('context', packet);
-    },
-
-    /**************
     func: log_question
     params: packet
     describe: this is the logging mechanism for system questions.
     ***************/
     log_question(packet) {
-      this.action('log_question');
       const p = this.copy(packet);
       const client = p.q.client.id;
       const agent = {
@@ -110,10 +49,6 @@ const LOG = new Deva({
         key: p.q.agent.key,
         name: p.q.agent.name,
       };
-      delete p.q.client;
-      delete p.q.agent;
-      delete p.a;
-
       p.client = client;
       p.agent = agent;
 
@@ -126,7 +61,6 @@ const LOG = new Deva({
     describe: this is the logging mechanism for system questions.
     ***************/
     log_answer(packet) {
-      this.action('log_answer');
       const p = this.copy(packet);
       const client = p.a.client.id;
       const agent = {
@@ -134,12 +68,6 @@ const LOG = new Deva({
         key: p.a.agent.key,
         name: p.a.agent.name,
       };
-
-      delete p.a.client;
-      delete p.a.agent;
-      delete p.q.client;
-      delete p.q.agent;
-
       p.client = client;
       p.agent = agent;
 
@@ -152,7 +80,6 @@ const LOG = new Deva({
     describe: this is the logging mechanism for system questions.
     ***************/
     log_ask(packet) {
-      this.action('log_ask');
       const p = this.copy(packet);
       const client = p.a.client.id;
       const agent = {
@@ -160,12 +87,6 @@ const LOG = new Deva({
         key: p.a.agent.key,
         name: p.a.agent.name,
       };
-
-      delete p.a.client;
-      delete p.a.agent;
-      delete p.q.client;
-      delete p.q.agent;
-
       p.client = client;
       p.agent = agent;
 
@@ -173,64 +94,53 @@ const LOG = new Deva({
     },
 
     log_write(type, packet) {
-      const packetstr = JSON.stringify(packet);
-      const pack = JSON.parse(packetstr);
-      const theDate = new Date();
-      const theMonth = theDate.getMonth() + 1;
+      const theUTC = Date.now();
+      const theDate = new Date(theUTC);
+      const tempMonth = theDate.getMonth() + 1;
+      const theMonth = tempMonth < 10 ? `0${tempMonth}` : tempMonth;
       const theYear = theDate.getFullYear();
-      const theAgent = pack.agent.key;
+      const theAgent = packet.agent.key;
       const theLoc = path.join('logs', 'devas', `${theAgent}`, `${type}`, `${theYear}`, `${theMonth}`)
 
       let theDir = path.join(__dirname, `${theLoc}`)
       if (this.config.dir) theDir = path.join(this.config.dir, theLoc);
-      const theFile = path.join(theDir, `${this.getToday()}.log`);
+      const theFile = path.join(theDir, `${theUTC}.json`);
       return new Promise((resolve, reject) => {
         fs.mkdir(theDir, {recursive:true}, err => {
           if (err) return this.error(err, pack, reject);
           // first check for file and if it does not then write the base file for the day
-          if (! fs.existsSync(theFile)) {
-            const header = [
-              `id: ${this.uid(true)}`,
-              `type: ${type}`,
-              `date: ${this.formatDate(theDate, 'long', true)}`,
-              `copyright: Copyright (c) ${theDate.getFullYear()} Quinn Michaels, All Rights Reserved.`,
-              '---',
-              '',
-            ].join('\n');
-            fs.writeFileSync(theFile, header, {encoding:'utf8',flag:'w'});
+          if (!fs.existsSync(theFile)) {
+            const saveJSON = {
+              id: this.uid(true),
+              type,
+              date: this.formatDate(theDate, 'long', true),
+              created: theDate,
+              copyright: `Copyright (c) ${theDate.getFullYear()} Quinn Michaels, All Rights Reserved.`,
+              DATA: packet,
+            }
+            fs.writeFileSync(theFile, JSON.stringify(saveJSON, null, 2), {encoding:'utf8',flag:'w'});
           }
-
-          // then after we check the file we are going to read the file and then add new data to the log.
-          const log_data = [
-            '',
-            '---',
-            `id: ${this.uid(true)}`,
-            `created: ${this.formatDate(Date.now(), 'short', true)}`,
-            `packet: ${packetstr}`,
-            `hash: ${this.hash(packetstr, 'sha512')}`,
-            '',
-          ].join('\n');
-          fs.appendFile(theFile, log_data, 'utf8', err => {
-            if (err) return this.error(err, pack, reject);
-          });
         });
       });
     },
   },
   methods: {},
   onDone(data) {
-    this.listen('devacore:state', packet => {
-      return this.func.log_state(packet);
-    });
-    this.listen('devacore:action', packet => {
-      return this.func.log_action(packet);
-    });
-    this.listen('devacore:feature', packet => {
-      return this.func.log_feature(packet);
-    });
-    this.listen('devacore:zone', packet => {
-      return this.func.log_zone(packet);
-    });
+    // this.listen('devacore:state', packet => {
+    //   return this.func.log_state(packet);
+    // });
+    // this.listen('devacore:action', packet => {
+    //   return this.func.log_action(packet);
+    // });
+    // this.listen('devacore:feature', packet => {
+    //   return this.func.log_feature(packet);
+    // });
+    // this.listen('devacore:zone', packet => {
+    //   return this.func.log_zone(packet);
+    // });
+    // this.listen('devacore:context', packet => {
+    //   return this.func.log_context(packet);
+    // });
     this.listen('devacore:question', packet => {
       return this.func.log_question(packet);
     });
@@ -239,9 +149,6 @@ const LOG = new Deva({
     });
     this.listen('devacore:ask', packet => {
       return this.func.log_ask(packet);
-    });
-    this.listen('devacore:context', packet => {
-      return this.func.log_context(packet);
     });
     this.listen('devacore:error', packet => {
       return this.func.log_error(packet);
