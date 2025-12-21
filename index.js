2892d9307fe0aab35ef1c93d7463d9c6a46d9ce9
Quinn Michaels <quinn@indra.ai>
2025-12-21 14:47:11 -0800

    Update Deva Core to 1.30.0

--- index.js
+++ index.js
@@ -1,10 +1,9 @@
 "use strict";
+// Log Deva
 // Copyright ©2000-2025 Quinn A Michaels; All rights reserved. 
 // Legal Signature Required For Lawful Use.
 // Distributed under VLA:18508198527557914092 LICENSE.md
 // Saturday, November 22, 2025 - 9:40:09 AM
-
-// Log Deva
 
 import Deva from '@indra.ai/deva';
 import {MongoClient} from 'mongodb';
